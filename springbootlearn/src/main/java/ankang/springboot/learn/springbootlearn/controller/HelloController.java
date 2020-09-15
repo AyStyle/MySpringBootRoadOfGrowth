61f76234b3d74eb53f7b7d6693a765d429b9c9c0
AyStyle <dreedisgood@qq.com>
2020-09-16 01:06:31 +0800

    SpringBoot Demo

---
+++ springbootlearn/src/main/java/ankang/springboot/learn/springbootlearn/controller/HelloController.java
@@ -0,0 +1,20 @@
+package ankang.springboot.learn.springbootlearn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ankang
+ * @email: dreedisgood@qq.com
+ * @create: 2020-09-16
+ */
+// @RestController = @ResponseBody + @Controller
+@RestController
+public class HelloController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "Hello Spring Boot";
+    }
+
+}
