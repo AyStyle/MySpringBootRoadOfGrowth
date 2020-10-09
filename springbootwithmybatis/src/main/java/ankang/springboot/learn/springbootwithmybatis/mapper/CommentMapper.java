3b9b47456754a23e86ddeaccecc71bc7fc65c475
ankang <ankang@soyoung.com>
2020-10-09 13:03:02 +0800

    修改注释

--- springbootwithmybatis/src/main/java/ankang/springboot/learn/springbootwithmybatis/mapper/CommentMapper.java
+++ springbootwithmybatis/src/main/java/ankang/springboot/learn/springbootwithmybatis/mapper/CommentMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
  * @email: dreedisgood@qq.com
  * @create: 2020-09-22
  */
-@Mapper // 表标识该接口是Mybatis的接口文件，并且让SpringBoot能够扫描到该接口，生成该接口的代理对象，存到容器中
+@Mapper // 标识该接口是Mybatis的接口文件，并且让SpringBoot能够扫描到该接口，生成该接口的代理对象，存到容器中
 public interface CommentMapper {
 
     @Select("SELECT * FROM springbootdata.t_comment WHERE id = #{id}")
